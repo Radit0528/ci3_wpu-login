e13a377ec5aa8800beefa6022d0f1363dc1a24b0
Radit0528 <3103120231@student.smktelkom-pwt.sch.id>
2022-09-14 10:35:16 +0700

    first commit

---
+++ application/controllers/Auth_radit.php
@@ -0,0 +1,127 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Auth_radit extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('form_validation');
+    }
+
+    public function index()
+    {
+        if ($this->session->userdata('email')) {
+            redirect('user_radit');
+        }
+        $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
+        $this->form_validation->set_rules('password', 'Password', 'trim|required');
+        if ($this->form_validation->run() == false) {
+            $data['title'] = 'Login Page';
+            $this->load->view('templates/auth_header_radit', $data);
+            $this->load->view('auth/login_radit');
+            $this->load->view('templates/auth_footer_radit');
+        } else {
+            //validasi sukses
+            $this->_login();
+        }
+    }
+
+    private function _login()
+    {
+        $email = $this->input->post('email');
+        $password = $this->input->post('password');
+
+        $user = $this->db->get_where('user', ['email' => $email])->row_array();
+
+        //jika usernya ada
+        if ($user != null) {
+            //jika usernya aktif
+            if ($user['is_active'] == 1) {
+                //cek password
+                if (password_verify($password, $user['password'])) {
+                    $data = [
+                        'email' => $user['email'],
+                        'role_id' => $user['role_id']
+                    ];
+                    $this->session->set_userdata($data);
+                    if ($user['role_id'] == 1) {
+                        redirect('admin_radit');
+                    } else {
+                        redirect('user_radit');
+                    }
+                } else {
+                    $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">
+                    Wrong password!
+                    </div>');
+                    redirect('auth_radit');
+                }
+            } else {
+                $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">
+                This email has not been activated!
+                </div>');
+                redirect('auth_radit');
+            }
+        } else {
+            $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">
+            Email is not registered!
+            </div>');
+            redirect('auth_radit');
+        }
+    }
+
+    public function registration()
+    {
+        if ($this->session->userdata('email')) {
+            redirect('user_radit');
+        }
+        $this->form_validation->set_rules('name', 'Name', 'required|trim');
+        $this->form_validation->set_rules('email', 'Email', 'required|trim|valid_email|is_unique[user.email]', [
+            'is_unique' => 'This email has already registered!'
+        ]);
+        $this->form_validation->set_rules('password1', 'Password', 'required|trim|min_length[3]|matches[password2]', [
+            'matches' => 'Password dont match!',
+            'min_length' => 'Password too short!'
+        ]);
+        $this->form_validation->set_rules('password2', 'Password', 'required|trim|matches[password1]');
+
+        if ($this->form_validation->run() == false) {
+            $data['title'] = 'WPU User Registration';
+            $this->load->view('templates/auth_header_radit', $data);
+            $this->load->view('auth/registration_radit');
+            $this->load->view('templates/auth_footer_radit');
+        } else {
+            $data = [
+                'name' => htmlspecialchars($this->input->post('name', true)),
+                'email' => htmlspecialchars($this->input->post('email', true)),
+                'image' => 'default.jpg',
+                'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
+                'role_id' => 2,
+                'is_active' => 1,
+                'date_created' => time()
+            ];
+
+            $this->db->insert('user', $data);
+            $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
+            Congratulation! your account has been created. Please Login 
+            </div>');
+            redirect('auth_radit');
+        }
+    }
+
+    public function logout()
+    {
+        $this->session->unset_userdata('email');
+        $this->session->unset_userdata('role_id');
+
+        $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
+        You has been logged out! 
+        </div>');
+        redirect('auth_radit');
+    }
+
+    public function blocked()
+    {
+        $this->load->view('auth/blocked_radit');
+    }
+}
